8669d4d97ac949fb417c1999256b5e0bc94060a9
Eduardo Pérez <educodes0@gmail.com>
2021-06-18 19:04:41 -0500

    Add cors and middlewares

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 const express = require('express')
+const cors = require('cors')
+const logger = require('./LoggerMiddleware')
 const app = express()
 const PORT = 3001
 app.listen(PORT)
 
 app.use(express.json())
+app.use(cors())
+
+app.use(logger)
 
 let notes = [
   {
     id: 1,
+    title: 'HTML',
     content: 'HTML is easy',
     date: '2019-05-30T17:30:31.098Z',
     important: false
   },
   {
     id: 2,
+    title: 'HTML',
     content: 'Browser can execute only JavaScript',
     date: '2019-05-30T18:39:34.091Z',
     important: false
   },
   {
     id: 3,
+    title: 'HTML',
     content: 'GET and POST are the most important methods of HTTP protocol',
     date: '2019-05-30T19:20:14.298Z',
     important: false
   },
   {
     id: 4,
+    title: 'HTML',
     content: 'GET and POST are the most important methods of HTTP protocol',
     date: '2019-05-30T19:20:14.298Z',
     important: false
@@ -68,7 +77,7 @@ app.delete('/notes/:id', (req, res) => {
 app.post('/notes/', (request, response) => {
   const getNote = request.body
 
-  if (!getNote || !getNote.content) {
+  if (!getNote || !getNote.content || !getNote.title) {
     return response.status(400).json({
       error: 'getNote.content is missing'
     })
@@ -79,6 +88,7 @@ app.post('/notes/', (request, response) => {
 
   const newNote = {
     id: idMax + 1,
+    title: getNote.title,
     content: getNote.content,
     important:
       typeof getNote.important !== 'undefined' ? getNote.important : false,
@@ -87,4 +97,10 @@ app.post('/notes/', (request, response) => {
   // notes = [...notes, newNote]
   notes = notes.concat(newNote)
   response.status(201).json(newNote)
+})
+
+app.use((request, response) => {
+  response.status(404).json({
+    error: 'Not Found Page : 404'
+  })
 })
